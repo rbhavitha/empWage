a8e2555d4e613b89e7d8a5cad6910911de3c0f90
rbhavitha <bhavitharedrouthu@gmail.com>
2020-10-05 21:12:06 +0530

    daily employee wage calculated

--- empAttendance.sh
+++ empAttendance.sh
@@ -4,7 +4,16 @@ isPresent=1;
 randomCheck=$(( RANDOM%2 ));
 if [ $isPresent -eq $randomCheck ];
 then
-        echo "Employee is present";
+        echo "Employee is Present";
 else
-        echo "Employee is absent":
+        echo "Employee is Absent":
+fi
+
+if [ $isPresent -eq $randomCheck ]
+then
+	empRatePerHr=20
+	empHrs=8
+	salary=$(($empHrs*$empRatePerHr))
+else
+	salary=0
 fi
